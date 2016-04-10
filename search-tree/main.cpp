b1831cc2ff28bbdb6ea2a0a58822f6bfa6d60201
jaken herman <jakenherman7@gmail.com>
2016-04-10 15:06:12 -0500

    Initial Commit

---
+++ search-tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  search-tree
+//
+//  Created by Computer Science on 4/10/16.
+//  Copyright © 2016 jaken herman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
